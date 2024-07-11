50472200a209006f8a7e8b14024f160c37ea27dd
юрий ткаченко <tkahenkaura@gmail.com>
2024-07-11 14:47:13 +0300

    123

---
+++ task111111/interfaces/ICollection.cs
@@ -0,0 +1,13 @@
+﻿namespace DataStructuresLib.Interfaces
+{
+    public interface ICollection
+    {
+        int Count { get; }
+        void Add(object item);
+        bool Remove(object item);
+        void Clear();
+        bool Contains(object item);
+        object[] ToArray();
+    }
+}
+
